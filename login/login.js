4a8f52a5273edbe2ada9cd41e0a3ce05d913f69e
nana <eyzzz_@naver.com>
2022-03-24 19:41:52 +0900

    20220324

---
+++ login/login.js
@@ -0,0 +1,9 @@
+function Home() {
+    <div className={styles.Wrapper}>
+        <h1>Our__</h1>
+        <label className={styles.label}>아이디</label>
+        <input className={styles.input} type="text"></input>
+        <label className={styles.label}>비밀번호</label>
+        <input className={styles.input} type="password" />
+    </div>
+}
